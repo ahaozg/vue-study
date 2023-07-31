213f9a7ad4ace28f2aa8705bfbeb09f4c1c64bed
haozg-666 <106981170+haozg-666@users.noreply.github.com>
2023-07-31 22:45:03 +0800

    模板处理

--- vue2/kvue/kvue.js
+++ vue2/kvue/kvue.js
@@ -1,37 +1,91 @@
-
 function defineReactive(obj, key, val) {
-    observe(val);
-    Object.defineProperty(obj, key, {
-        get() {
-            console.log('get', key);
-            return val;
-        },
-        set(v) {
-            if (v !== val) {
-                val = v;
-                observe(v);
-                console.log('set', key, val);
-                // update(val);
-            }
-        },
+  observe(val);
+  Object.defineProperty(obj, key, {
+    get() {
+      console.log('get', key);
+      return val;
+    },
+    set(v) {
+      if (v !== val) {
+        val = v;
+        observe(v);
+        console.log('set', key, val);
+        // update(val);
+      }
+    },
 
-    })
+  })
 }
 
 function observe(obj) {
-    if (typeof obj !== 'object' || obj === null) {
-        return obj;
-    }
-    Object.keys(obj).forEach(key => defineReactive(obj, key, obj[key]));
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+  Object.keys(obj).forEach(key => defineReactive(obj, key, obj[key]));
+}
+
+function proxy(vm) {
+  Object.keys(vm.$data).forEach(key => {
+    Object.defineProperty(vm, key, {
+      get() {
+        return vm.$data[key];
+      },
+      set(val) {
+        vm.$data[key] = val;
+      }
+    })
+  })
 }
 
 class Kvue {
-    constructor(options) {
-        // 1.保存选项
-        this.$options = options;
-        this.$data = options.data;
-        // 2.对data选项做响应式处理
-        observe(this.$data);
-        // 3.编译
-    }
+  constructor(options) {
+    // 1.保存选项
+    this.$options = options;
+    this.$data = options.data;
+    // 2.对data选项做响应式处理
+    observe(this.$data);
+    // 2.5代理
+    proxy(this);
+    // 3.编译
+    new Compile(options.el, this);
+  }
+}
+
+class Compile {
+  constructor(el, vm) {
+    // 保存Kvue实例
+    this.$vm = vm;
+    // 编译模板树
+    this.compile(document.querySelector(el));
+  }
+
+  // el模板根节点
+  compile(el) {
+    // 遍历el
+    // 1.获取el所有子节点
+    el.childNodes.forEach(node => {
+      if (node.nodeType === 1) {
+        // 元素
+        console.log('element', node.nodeName);
+        // 继续递归
+        if (node.childNodes.length > 0) {
+          this.compile(node);
+        }
+      } else if (this.isInter(node)) {
+        // 插值文本
+        console.log('text', node.textContent);
+        this.compileText(node);
+      }
+    })
+  }
+
+  // 处理插值文本
+  compileText(node) {
+    node.textContent = this.$vm[RegExp.$1];
+  }
+
+  // {{xxxx}}
+  isInter(node) {
+    return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent);
+  }
 }

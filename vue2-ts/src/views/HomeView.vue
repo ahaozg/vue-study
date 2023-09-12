bf5f888b08d1f515cf61c117e1949bbd0ef70357
haozg <106981170+haozg-666@users.noreply.github.com>
2023-09-12 22:38:48 +0800

    feat: vue2使用ts的三种方式

--- vue2-ts/src/views/HomeView.vue
+++ vue2-ts/src/views/HomeView.vue
@@ -2,16 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld1 />
+    <HelloWorld2 msg="hahahha" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld1 from '@/components/HelloWorld1.vue';
+import HelloWorld2 from '@/components/HelloWorld2';
 
 @Component({
   components: {
     HelloWorld,
+    HelloWorld1,
+    HelloWorld2,
   },
 })
 export default class HomeView extends Vue {}

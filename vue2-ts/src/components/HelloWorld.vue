602ffa7011023dba7287bee29a48456476892ab9
haozg <106981170+haozg-666@users.noreply.github.com>
2023-09-14 21:42:15 +0800

    feat: vue2+ts

--- vue2-ts/src/components/HelloWorld.vue
+++ vue2-ts/src/components/HelloWorld.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>
+      <input type="text" @keydown.enter="addFeature">
+    </p>
     <ul>
       <li v-for="feature in features"
           :class="{selected: feature.selected}"
           :key="feature.id">
         {{feature.name}}
       </li>
+      <li>总数：{{count}}</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-property-decorator';
+import Axios from 'axios';
 
 // 类型别名
 type Feature = {
@@ -20,15 +25,50 @@ type Feature = {
   name: string;
 }
 
+// 交叉类型
 type FeatureSelect = Feature & {selected: boolean}
+
+interface Result<T> {
+  ok: number;
+  data: T;
+}
+
+// 泛型方法
+function getResult<T>(): Promise<Result<T>> {
+  const data: any = [
+    {id: 1, name: '类型注解', selected: false},
+    {id: 2, name: '编译形语言', selected: true},
+  ]
+  return Promise.resolve({
+    ok: 0 | 1,
+    data
+  });
+}
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
-  features: FeatureSelect[] = [
-    {id: 1, name: '类型注解', selected: false},
-    {id: 2, name: '编译形语言', selected: true},
-  ]
+  features: FeatureSelect[] = [];
+
+  async created() {
+    this.features = (await getResult<FeatureSelect[]>()).data;
+    Axios.get('/api/list')
+  }
+
+  addFeature(e: KeyboardEvent) {
+    const input = e.target as HTMLInputElement;
+    const feature: FeatureSelect = {
+      id: this.features.length + 1,
+      name: input.value,
+      selected: false
+    }
+    this.features.push(feature);
+    input.value = '';
+  }
+
+  get count() {
+    return this.features.length
+  }
 }
 </script>
 
--- vue2-ts/vue.config.js
+++ vue2-ts/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    before(app) {
+      app.get("/api/list", (req, res) => {
+        res.json([
+          { id: 1, name: "类型注解", selected: true },
+          { id: 2, name: "编译型语言", selected: false },
+        ]);
+      });
+    },
+  },
 })
